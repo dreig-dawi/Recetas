c7f322b32ace1d197a153e4546a6c67823b81d56
dreig-dawi <dreigbuades@cifpfbmoll.eu>
2024-11-05 17:32:20 +0100

    Added: test.php

--- test.php
+++ test.php
@@ -1,29 +1,19 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 
-include_once '../database.php';
+include_once 'database.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-try {
 	if ( $method == 'DELETE' ) {
 		$data = json_decode( file_get_contents( 'php://input' ), TRUE );
 		$sql  = "DELETE FROM recetas WHERE Plato = :plato";
 		$stmt = $db->prepare( $sql );
 		$stmt->bindParam( ':plato', $data['Plato'] );
 		if ( $stmt->execute() ) {
-			echo json_encode( [ 'mensaje' => 'Receta eliminada' ] );
-		}
-		else {
-			echo json_encode( [ 'mensaje' => 'Error al eliminar la receta' ] );
+			echo json_encode( [ 'mensaje' => $stmt ] );
 		}
 	}
-	else {
-		echo json_encode( [ 'mensaje' => 'Método no soportado' ] );
-	}
-} catch ( Exception $e ) {
-	echo json_encode( [ 'mensaje' => 'Algo no ha salido bien...' ] );
-}
